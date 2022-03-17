f51a6a7c710f3d61432b9c51da69997f9d3a1ce3
Mike543 <p890098@gmail.com>
2022-03-17 11:40:58 +0300

    feat: add skills

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skill__percent-count'),
+    lines= document.querySelectorAll('.skill__percent-lineholder span');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+})
